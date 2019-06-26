5825dfef502933b79c11af221e69885c7ab9889c
sn2234 <sn2234@gmail.com>
2019-06-27 00:18:04 +0300

    Add some scripts for visualization

--- demo_predict.py
+++ demo_predict.py
@@ -6,13 +6,11 @@ os.environ['PROJ_LIB'] = r'C:\Users\nobody\Anaconda3\envs\cntk\Lib\site-packages
 
 import numpy as np
 import pandas as pd
-import os, argparse, cntk, tifffile, warnings, osr
-from osgeo import gdal
-from gdalconst import *
-from mpl_toolkits.basemap import Basemap
+import os, argparse, cntk, tifffile, warnings
 from collections import namedtuple
 from PIL import Image
 import matplotlib.pyplot as plt
+from scipy.special import expit
 
 #%% Load the model and evaluation data
 # The default filename points to your model trained for one epoch.
@@ -29,44 +27,26 @@ with warnings.catch_warnings():
     true_lc_image = np.transpose(tifffile.imread(lc_filename))
 true_lc_image[true_lc_image > 4] = 4
 
-gdal.UseExceptions()
-
-#%% Find the relevant subregion of the image
-os.environ['PROJ_LIB'] = r'C:\Users\nobody\Anaconda3\envs\cntk\Library\share\proj'
-def find_pixel_from_latlon(img_filename, lat, lon):
-    ''' Find the indices for a point of interest '''
-    img = gdal.Open(img_filename, GA_ReadOnly)
-    img_proj = osr.SpatialReference()
-    img_proj.ImportFromWkt(img.GetProjection())
-    ulcrnrx, xstep, _, ulcrnry, _, ystep = img.GetGeoTransform()
-
-    world_map = Basemap(lat_0=0,
-                        lon_0=0,
-                        llcrnrlat=-90, urcrnrlat=90,
-                        llcrnrlon=-180, urcrnrlon=180,
-                        resolution='c', projection='stere')
-    world_proj = osr.SpatialReference()
-    world_proj.ImportFromProj4(world_map.proj4string)
-    ct_to_img = osr.CoordinateTransformation(world_proj, img_proj)
-
-    xpos, ypos = world_map(lon, lat, inverse=False)
-    xpos, ypos, _ = ct_to_img.TransformPoint(xpos, ypos)
-    x = int((xpos - ulcrnrx) / xstep)
-    y = int((ypos - ulcrnry) / ystep)
-
-    return(x,y)
+#%% Choose random sample from original image
 
 region_dim = 256
 delta = int(region_dim / 2)
 padding = 64
+_, imgHeight, imgWidth = naip_image.shape
 
-center_x, center_y = find_pixel_from_latlon(naip_filename, 37.055522, -78.638640)
-true_lc_image = true_lc_image[center_x - delta:center_x + delta,
-                              center_y - delta:center_y + delta].astype(np.float32)
-naip_image = naip_image[:,
+center_x = np.random.randint(padding+delta, imgWidth-(padding+delta))
+center_y = np.random.randint(padding+delta, imgHeight-(padding+delta))
+
+
+#%% Extract patch
+
+dataPatch = naip_image[:,
                         center_x - (delta + padding):center_x + delta + padding,
                         center_y - (delta + padding):center_y + delta + padding].astype(
                             np.float32)
+
+groundTruthPatch = true_lc_image[center_x - delta:center_x + delta,
+                              center_y - delta:center_y + delta].astype(np.float32)
 
 #%% Applying the model across the region of interest
 n_rows = int(region_dim / 128)
@@ -74,12 +54,12 @@ n_rows = int(region_dim / 128)
 # The model's predictions will have five values for each x-y position:
 # these can be used to find the relative predicted probabilities of
 # each of the five labels.
-pred_lc_image = np.zeros((5, true_lc_image.shape[0], true_lc_image.shape[1]))
+predictedPatch = np.zeros((5, groundTruthPatch.shape[0], groundTruthPatch.shape[1]))
 
 for row_idx in range(n_rows):
     for col_idx in range(n_rows):
         # Extract a 256 x 256 region from the NAIP image, to feed into the model.
-        sq_naip = naip_image[:,
+        sq_naip = dataPatch[:,
                              row_idx * 128:(row_idx * 128) + 256,
                              col_idx * 128:(col_idx * 128) + 256]
         
@@ -87,19 +67,11 @@ for row_idx in range(n_rows):
         sq_pred_lc = np.squeeze(model.eval({model.arguments[0]: [sq_naip]}))
         
         # Save the predictions in the appropriate region of the result matrix
-        pred_lc_image[:,
+        predictedPatch[:,
                       row_idx * 128:(row_idx * 128) + 128,
                       col_idx * 128:(col_idx * 128) + 128] = sq_pred_lc
 
-# You may disregard the RuntimeWarning about non-contiguous data
-
-#%% Draw results
-
-fig = plt.figure(figsize=(10, 40))
-layers = pred_lc_image.shape[0]
-total = layers+2
-
-threshold = pred_lc_image.mean(axis=(1,2)) * 1.1
+#%% Visualization
 
 layerNames = [
     "No data",
@@ -108,23 +80,6 @@ layerNames = [
     "Herbaceous",
     "Barren/impervious"
 ]
-
-def showImg(img, n, title):
-    sub = fig.add_subplot(total, 2, n)
-    sub.imshow(img)
-    sub.set_xticks([])
-    sub.set_yticks([])
-    sub.set_title(title)
-
-showImg(naip_image[:3].transpose((1,2,0)), 1, "Image")
-
-for n in range(layers):
-    start = 1+2*(n+total-layers)
-    showImg(pred_lc_image[n] > threshold[n], start, f"Prediction {layerNames[n]}")
-    showImg(true_lc_image == n, start+1, f"Ground truth {layerNames[n]}")
-
-
-#%% Visualization
 
 color_map = np.asarray([[0,0,0],
                         [0,0,1],
@@ -150,20 +105,43 @@ def visualize_label_image(input_image, hard=True):
     label_image = np.transpose(label_image * 255).astype(np.uint8)
     return(label_image)
 
+fig = plt.figure(figsize=(10, 40))
+layers = predictedPatch.shape[0]
+total = layers+2
+
+def showImg(img, n, title):
+    sub = fig.add_subplot(total, 1, n)
+    sub.imshow(img)
+    sub.set_xticks([])
+    sub.set_yticks([])
+    sub.set_title(title)
+
+showImg(dataPatch[:3].transpose((2,1,0)), 1, "Image")
+# Draw legend
+
+
 #%% Predicted land cover labels
-img_labels_pred = visualize_label_image(pred_lc_image, hard=True)
+img_labels_pred = visualize_label_image(predictedPatch, hard=True)
 plt.imshow(img_labels_pred)
 
 #%% Ground-truth land cover labels
-true_lc_labels = np.transpose(np.eye(5)[true_lc_image.astype(np.int32)], [2, 0, 1])
+true_lc_labels = np.transpose(np.eye(5)[groundTruthPatch.astype(np.int32)], [2, 0, 1])
 img_labels_true = visualize_label_image(true_lc_labels, hard=True)
 plt.imshow(img_labels_true)
 
-#%% Quantifying accuracy
-fraction_correct = np.sum(true_lc_image == pred_lc_image.argmax(axis=0)) / region_dim**2
-print('{:.1f}% of best-guess predictions were correct'.format(100 * fraction_correct))
 
-pred_softmax = np.exp(pred_lc_image) / np.sum(np.exp(pred_lc_image), axis=0)
-avg_prob_correct_label = np.sum(np.multiply(true_lc_labels, pred_softmax)) / region_dim**2
-print('Average probability assigned to the true label: {:.1f}%'.format(
-    100 * avg_prob_correct_label))
+#%% Draw legend
+fig = plt.figure(figsize=(10, 10))
+total = len(layerNames)
+
+def showImg(img, n, title):
+    sub = fig.add_subplot(total, 1, n)
+    sub.imshow(img)
+    sub.set_xticks([])
+    sub.set_yticks([])
+    sub.set_title(title)
+
+for i in range(total):
+    mapImg = np.full((30, 30, 3), color_map[i])
+    showImg(mapImg, i+1, layerNames[i])
+
---
+++ demo_predict_overlay.py
@@ -0,0 +1,156 @@
+
+#%% Load dependencies
+
+import os
+os.environ['PROJ_LIB'] = r'C:\Users\nobody\Anaconda3\envs\cntk\Lib\site-packages\pyproj\data'
+
+import numpy as np
+import pandas as pd
+import cv2
+import os, argparse, cntk, tifffile, warnings
+from collections import namedtuple
+import matplotlib.pyplot as plt
+from scipy.special import expit
+
+#%% Load the model and evaluation data
+# The default filename points to your model trained for one epoch.
+# You can also try using our sample model, 250epochs.model
+model_filename = r"D:\Prog\Projects\MachineLearning\Misc\Datasets\pixellevellandclassification\models\250epochs.model"
+model = cntk.load_model(model_filename)
+
+naip_filename = r"D:\Prog\Projects\MachineLearning\Misc\Datasets\pixellevellandclassification\evaluation_data\C14_NAIP.tif"
+lc_filename = naip_filename.replace('_NAIP.tif', '_LandCover.tif')
+
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore')
+    naip_image = np.transpose(tifffile.imread(naip_filename))  / 255.0
+    true_lc_image = np.transpose(tifffile.imread(lc_filename))
+true_lc_image[true_lc_image > 4] = 4
+
+#%% Choose random sample from original image
+
+region_dim = 256
+delta = int(region_dim / 2)
+padding = 64
+_, imgHeight, imgWidth = naip_image.shape
+
+center_x = np.random.randint(padding+delta, imgWidth-(padding+delta))
+center_y = np.random.randint(padding+delta, imgHeight-(padding+delta))
+
+
+#%% Extract patch
+
+dataPatch = naip_image[:,
+                        center_x - (delta + padding):center_x + delta + padding,
+                        center_y - (delta + padding):center_y + delta + padding].astype(
+                            np.float32)
+
+groundTruthPatch = true_lc_image[center_x - delta:center_x + delta,
+                              center_y - delta:center_y + delta].astype(np.float32)
+
+#%% Applying the model across the region of interest
+n_rows = int(region_dim / 128)
+
+# The model's predictions will have five values for each x-y position:
+# these can be used to find the relative predicted probabilities of
+# each of the five labels.
+predictedPatch = np.zeros((5, groundTruthPatch.shape[0], groundTruthPatch.shape[1]))
+
+for row_idx in range(n_rows):
+    for col_idx in range(n_rows):
+        # Extract a 256 x 256 region from the NAIP image, to feed into the model.
+        sq_naip = dataPatch[:,
+                             row_idx * 128:(row_idx * 128) + 256,
+                             col_idx * 128:(col_idx * 128) + 256]
+        
+        # Get the model's prediction for the center of that region
+        sq_pred_lc = np.squeeze(model.eval({model.arguments[0]: [sq_naip]}))
+        
+        # Save the predictions in the appropriate region of the result matrix
+        predictedPatch[:,
+                      row_idx * 128:(row_idx * 128) + 128,
+                      col_idx * 128:(col_idx * 128) + 128] = sq_pred_lc
+
+#%% Visualization
+
+layerNames = [
+    "No data",
+    "Water",
+    "Trees",
+    "Herbaceous",
+    "Barren/impervious"
+]
+
+color_map = np.asarray([[0,0,0],
+                        [0,0,1],
+                        [0,0.5,0],
+                        [0.5,1,0.5],
+                        [0.5,0.375,0.375]], dtype=np.float32)
+
+def visualize_label_image(input_image, hard=True):
+    num_labels, height, width = input_image.shape
+    label_image = np.zeros((3, height, width))
+    if hard:
+        my_label_indices = input_image.argmax(axis=0)
+        for label_idx in range(num_labels):
+            for rgb_idx in range(3):
+                label_image[rgb_idx, :, :] += (my_label_indices == label_idx) *\
+                    color_map[label_idx, rgb_idx]
+    else:
+        input_image = np.exp(input_image) / np.sum(np.exp(input_image), axis=0)
+        for label_idx in range(num_labels):
+            for rgb_idx in range(3):
+                label_image[rgb_idx, :, :] += input_image[label_idx, :, :] * \
+                    color_map[label_idx, rgb_idx]
+    label_image = np.transpose(label_image * 255).astype(np.uint8)
+    return(label_image)
+
+fig = plt.figure(figsize=(10, 30))
+total = 3
+
+def showImg(img, n, title):
+    sub = fig.add_subplot(total, 1, n)
+    sub.imshow(img)
+    sub.set_xticks([])
+    sub.set_yticks([])
+    sub.set_title(title)
+
+img_labels_pred = visualize_label_image(predictedPatch, hard=True)
+true_lc_labels = np.transpose(np.eye(5)[groundTruthPatch.astype(np.int32)], [2, 0, 1])
+img_labels_true = visualize_label_image(true_lc_labels, hard=True)
+
+img_shape = (region_dim+2*padding, region_dim+2*padding, 3)
+
+img_labels_pred_full = np.zeros(img_shape, dtype="float32")
+img_labels_pred_full[padding:(region_dim+padding),
+                     padding:(region_dim+padding), :] = img_labels_pred
+overlayApplied = cv2.addWeighted(dataPatch[:3].transpose((2,1,0)), 1.0,
+                                 img_labels_pred_full, 0.5, 0)
+
+img_labels_true_full = np.zeros(img_shape, dtype="float32")
+img_labels_true_full[padding:(region_dim+padding),
+                     padding:(region_dim+padding), :] = img_labels_true
+overlayAppliedTrue = cv2.addWeighted(dataPatch[:3].transpose((2,1,0)), 1.0,
+                                 img_labels_true_full, 0.5, 0)
+
+
+showImg(dataPatch[:3].transpose((2,1,0)), 1, "Original Image")
+showImg(overlayApplied, 2, "Image with mask")
+showImg(overlayAppliedTrue, 3, "Image with manual markdown")
+#plt.imshow(img_labels_pred_full)
+
+#%% Draw legend
+fig = plt.figure(figsize=(5, 5))
+total = len(layerNames)
+
+def showImg(img, n, title):
+    sub = fig.add_subplot(total, 1, n)
+    sub.imshow(img)
+    sub.set_xticks([])
+    sub.set_yticks([])
+    sub.set_title(title)
+
+for i in range(total):
+    mapImg = np.full((30, 30, 3), color_map[i])
+    showImg(mapImg, i+1, layerNames[i])
+
